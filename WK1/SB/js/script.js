945ef7be66d0aa72c91fc7b3db4505cc96cb4772
Mirajo <mmj_chicago@yahoo.com>
2014-05-13 00:01:14 -0400

    var phrase
    var phrase

--- WK1/SB/js/script.js
+++ WK1/SB/js/script.js
@@ -6,3 +6,5 @@ var age = 38; //Number variable
 var isStudent = false; //Boolean Variable
 
 var firstName = "Kermit";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor' //special character slash
