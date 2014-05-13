b8368a30e05495cf4aeeca97b29c5fdb0547f0bd
Mirajo <mmj_chicago@yahoo.com>
2014-05-13 12:51:24 -0400

    console log area
    console log area

--- WK1/PAP/js/script.js
+++ WK1/PAP/js/script.js
@@ -3,3 +3,5 @@
 
 var width = prompt("We are calculating the area of a rectangle. Please enter the width")
 var height = prompt("Please enter the height")
+var area = width * height;
+console.log(area);
