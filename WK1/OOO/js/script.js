8bb8e0604a5c7daf30e5bf1bcd15e809e71a16ce
Mirajo <mmj_chicago@yahoo.com>
2014-05-13 02:19:31 -0400

    adding quizs
    adding quizs

--- WK1/OOO/js/script.js
+++ WK1/OOO/js/script.js
@@ -1,7 +1,10 @@
-//Expressions - Modulo Operator
+//Expressions - PEMDAS - The Order of Operations
 
+var quiz1 = 87;
+var quiz2 = 100;
+var quize3 = 60;
+var quiz4 = 80;
 
-
-
-
+//the average is the sum of the items divided by the number of items
+var average = quiz1 + quiz2 + quiz3 + quiz4;
 
