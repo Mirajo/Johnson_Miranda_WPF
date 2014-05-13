01437cf40003bef09f538970874c57c88161b23d
Mirajo <mmj_chicago@yahoo.com>
2014-05-13 04:34:43 -0400

    string casting
    string casting

--- WK1/casting/js/script.js
+++ WK1/casting/js/script.js
@@ -4,5 +4,5 @@ var areaCode = 407;
 var firstPart = 203;
 var secPart = 4529;
 
-var phoneNo = areaCode + firstpart + secPart;
+var phoneNo = String(areaCode) + firstPart + secPart;
 console.log(phoneNo);
