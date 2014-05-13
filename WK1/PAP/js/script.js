8e400587a59c53273afb512297a3e3c69cb8b552
Mirajo <mmj_chicago@yahoo.com>
2014-05-13 13:00:05 -0400

    alert results
    alert with user input shown

--- WK1/PAP/js/script.js
+++ WK1/PAP/js/script.js
@@ -6,4 +6,5 @@ var height = prompt("Please enter the height")
 var area = width * height; //calculates with that info 
 //console.log(area); //prints it out to the console
 
-alert("The area of your rectangle is" + area); 
+var result = "The area of your rectangle is " + area + "sq feet";
+alert(result); 
