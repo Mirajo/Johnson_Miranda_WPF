725b556414043baf4a0735b32659fa42b1f25557
Mirajo <mmj_chicago@yahoo.com>
2014-05-13 12:53:26 -0400

    area of rectangle
    area of rectangle

--- WK1/PAP/js/script.js
+++ WK1/PAP/js/script.js
@@ -3,5 +3,7 @@
 
 var width = prompt("We are calculating the area of a rectangle. Please enter the width")
 var height = prompt("Please enter the height")
-var area = width * height;
-console.log(area);
+var area = width * height; //calculates with that info 
+//console.log(area); //prints it out to the console
+
+alert("The area of your rectangle is" + area); 
