802f36fb88607cf66e7f2727c1f20113aeaf73e8
Mirajo <mmj_chicago@yahoo.com>
2014-05-13 04:17:57 -0400

    full name console
    full name console

--- WK1/strings/js/script.js
+++ WK1/strings/js/script.js
@@ -1,6 +1,8 @@
-//Expressions - Expressions with Arrays
+//Expressions - Concatenating Strings
 
-var orangeBins = [234, 567, 883];
+var firstName = "Miranda";
+var lastName = "Johnson";
+var fullName = firstName + lastName;
 
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-console.log(total);
+console.log(fullName);
+
