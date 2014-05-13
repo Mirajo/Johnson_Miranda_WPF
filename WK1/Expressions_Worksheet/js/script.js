9bd70e97a3fcb6bdba881050703af15000a0456a
Mirajo <mmj_chicago@yahoo.com>
2014-05-13 17:34:00 -0400

    sparky's age
    sparky’s age

--- WK1/Expressions_Worksheet/js/script.js
+++ WK1/Expressions_Worksheet/js/script.js
@@ -6,7 +6,9 @@
 var dogYears;
 var humanYears = 1;
 var dogYears = humanYears * 7;
-console.log(dogYears);
+
+//print out sparky's age
+console.log("Sparky is " + humanYears + "human years old which is" + dogYears + "in dog years." );
 
 
 //Slice of pie pt.1
