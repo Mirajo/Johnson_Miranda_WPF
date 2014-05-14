db1672feb6ceaba142abdd1b5818e3d0804eba0e
Mirajo <mmj_chicago@yahoo.com>
2014-05-14 18:46:54 -0400

    slice of pizza part 1
    slice of pizza part 1

--- WK1/Expressions_Worksheet/js/script.js
+++ WK1/Expressions_Worksheet/js/script.js
@@ -13,7 +13,12 @@ console.log("Sparky is "  + humanYears +" " + "human years old which is" +" " + 
 
 //Slice of pie pt.1
 
+var spp = 8;
+var pap = 16;
+var po = 5;
 
+//print out number number of slices each person ate
+concole.log("Each person ate "   " slices of pizza at the party".
 
 
 //Slice of pie pt.2
