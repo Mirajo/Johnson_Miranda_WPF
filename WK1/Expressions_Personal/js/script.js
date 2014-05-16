d287307d41c58510ee7d84093358bd6a2e4b9a98
Mirajo <mmj_chicago@yahoo.com>
2014-05-16 04:36:08 -0400

    four titles added
    four titles added

--- WK1/Expressions_Personal/js/script.js
+++ WK1/Expressions_Personal/js/script.js
@@ -5,9 +5,19 @@
 
 //This is a calculator for DVD purchases
 
-//Dvd titles purchased
+alert ("How much have you spent on DVDs ?");
+//Dvd title 1 purchased
 var dvd1 = prompt("Enter Title 1");
 
 
+//Dvd title 2 purchased
+var dvd2 = prompt("Enter Title 2");
 
 
+
+//Dvd title 3 purchased
+var dvd3 = prompt("Enter Title 3");
+
+
+//Dvd title 4 purchased
+var dvd4 = prompt("Enter Title 1");
