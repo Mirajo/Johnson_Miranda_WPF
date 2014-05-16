bf3ac03aaecddab17a309a6bb86848637fc0c79a
Mirajo <mmj_chicago@yahoo.com>
2014-05-16 13:28:08 -0400

    added cost per year
    added cost per year

--- WK1/Expressions_Personal/js/script.js
+++ WK1/Expressions_Personal/js/script.js
@@ -7,19 +7,19 @@
 
 alert ("How much have you spent on DVDs ?");
 //Dvd title 1 purchased
-var dvd1 = prompt("Enter Title 1");
+var dvd1 = prompt("Enter Title of DVD 1");
 
 
 //Dvd title 2 purchased
-var dvd2 = prompt("Enter Title 2");
+var dvd2 = prompt("Enter Title of DVD 2");
 
 
 //Dvd title 3 purchased
-var dvd3 = prompt("Enter Title 3");
+var dvd3 = prompt("Enter Title of DVD 3");
 
 
 //Dvd title 4 purchased
-var dvd4 = prompt("Enter Title 4");
+var dvd4 = prompt("Enter Title of DVD  4");
 
 //Dvd title total 1
 var dvd1Ttl = prompt("Enter Dvd total 1");
@@ -33,11 +33,25 @@ var dvd3Ttl = prompt("Enter Dvd total 3");
 //Dvd title total 4
 var dvd4Ttl = prompt("Enter Dvd total 4");
 
+//Cost per year
+
+//Cost per year if you purchase once a month
+var dvd1Ttlyr = dvd1Ttl * 12;
+
+//Cost per year if you purchase once a month
+var dvd2Ttlyr = dvd2Ttl * 12;
+
+//Cost per year if you purchase once a month
+var dvd3Ttlyr = dvd3Ttl * 12;
+
+//Cost per year if you purchase once a month
+var dvd4Ttlyr = dvd4Ttl * 12;
+
+
 //Total of all Dvd's
 
 var dvdAll = dvd1Ttl +" "+ dvd2Ttl +" "+ dvd3Ttl +" "+ dvd4Ttl; 
-
-var dvdAll = (dvd1Ttl) + (dvd2Ttl) + (dvd3Ttl) + (dvd4Ttl);
+ 
 console.log("$" + dvdAll);
 
 console.log("You spent a total of $" +" "+ dvdAll +" "+ "on four dvds."); 
