c0491e860e6d9f7883eca9f90d896ccb3c696008
Mirajo <mmj_chicago@yahoo.com>
2014-05-16 18:42:01 -0400

    correction
    correction

--- WK1/Expressions_Industry/js/script.js
+++ WK1/Expressions_Industry/js/script.js
@@ -7,16 +7,18 @@
 
 
 //var day1Hours = prompt("Enter hours worked on Day 1");
-var day2Hours = prompt("Enter hours worked on Day 2");
-var day3Hours = prompt("Enter hours worked on Day 3");
-var day4Hours = prompt("Enter hours worked on Day 4");
-var day5Hours = prompt("Enter hours worked on Day 5");
-var day6Hours = prompt("Enter hours worked on Day 6");
-var day7Hours = prompt("Enter hours worked on Day 7");//
+
+//var day2Hours = prompt("Enter hours worked on Day 2");
+//var day3Hours = prompt("Enter hours worked on Day 3");
+//var day4Hours = prompt("Enter hours worked on Day 4");
+//var day5Hours = prompt("Enter hours worked on Day 5");
+//var day6Hours = prompt("Enter hours worked on Day 6");
+//var day7Hours = prompt("Enter hours worked on Day 7");
 
 var hoursWk = "day1"[0] = prompt("Enter hours worked on Day 1");
 
 
+console.log(hoursWk);
 
  //"day2", "day3", "day4", "day5", "day6", "day7"];
 
