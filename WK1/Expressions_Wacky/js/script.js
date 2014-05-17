aa3a16d2643e26f9baa0ebbf1500a925b5fef8a1
Mirajo <mmj_chicago@yahoo.com>
2014-05-16 22:47:54 -0400

    socks missing variable
    socks missing variable

--- WK1/Expressions_Wacky/js/script.js
+++ WK1/Expressions_Wacky/js/script.js
@@ -2,10 +2,14 @@
 //WPF - Expressions Wacky
 //May 15, 2014
 
+
 //Socks lost at the laundry mat
 
+var socks = 100; //declare socks variable
 
-
+var socksM = socks - 2; //Socks missing variable
+  
+console.log(socksM); //Print out Socks Missing
 
 
 
