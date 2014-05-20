6a5505fcf550932fa0b3c8a261fbaecd05133900
Mirajo <mmj_chicago@yahoo.com>
2014-05-20 02:45:08 -0400

    Conditional Assignment initial commit
    Conditional Assignment initial commit

---
+++ Wk2/Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -0,0 +1,33 @@
+//Conditionals Worksheet WPF Activity
+
+
+
+
+//Stuff Your Face I
+
+
+
+//Celsius to Fahrenheit converter
+
+
+
+//Last Chance to Gas
+
+
+
+//Grade Letter Calculator
+
+
+
+//Check the login
+
+
+
+//Tire Pressure I
+
+
+
+//Movie Ticket Price
+
+
+
