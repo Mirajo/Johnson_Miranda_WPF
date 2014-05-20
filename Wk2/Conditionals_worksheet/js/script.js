46e819765228d8e7bd46225087a0462f77d7e48a
Mirajo <mmj_chicago@yahoo.com>
2014-05-20 02:42:13 -0400

    conditionals js set up
    conditionals js set up

--- Wk2/Conditionals_worksheet/js/script.js
+++ Wk2/Conditionals_worksheet/js/script.js
@@ -1,41 +1,33 @@
-//Expressions Worksheet WPF Activity
+//Conditionals Worksheet WPF Activity
 
 
-//Dog years
-
-var dogYears;
-var humanYears = 1;
-var dogYears = humanYears * 7;
-
-//print out sparky's age
-console.log("Sparky is "  + humanYears +" " + "human years old which is" +" " + dogYears + " "+ "in dog years." );
 
 
-//Slice of pie pt.1
-
-var slices; 
-var spp = 8; //slices per pizza
-var pap = 16; //people at party
-var po = 5; //pizzas order
-
-var slices = spp * po / pap;
-
-//print out number number of slices each person ate
-console.log("Each person ate "+ slices +" slices of pizza at the party.");
+//Stuff Your Face I
 
 
-//Slice of pie pt.2
 
-var slices;
-var spp = 8; //slices per pizza
-var pap = 16; //people at party
-var po = 5; //pizzas order
-
-var spark = spp * po;
+//Celsius to Fahrenheit converter
 
 
-//Sparky got x slices of pizza
-console.log("Sparky got"+" " + spark + " " +"slices of pizza.");
+
+//Last Chance to Gas
+
+
+
+//Grade Letter Calculator
+
+
+
+//Check the login
+
+
+
+//Tire Pressure I
+
+
+
+//Movie Ticket Price
 
 
 
