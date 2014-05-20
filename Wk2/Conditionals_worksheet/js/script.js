8953ab79559642744f61e2e896cd1c0bf031c59e
Mirajo <mmj_chicago@yahoo.com>
2014-05-20 01:45:53 -0400

    Conditionals worksheet initial commit
    Conditionals worksheet initial commit

---
+++ Wk2/Conditionals_worksheet/js/script.js
@@ -0,0 +1,41 @@
+//Expressions Worksheet WPF Activity
+
+
+//Dog years
+
+var dogYears;
+var humanYears = 1;
+var dogYears = humanYears * 7;
+
+//print out sparky's age
+console.log("Sparky is "  + humanYears +" " + "human years old which is" +" " + dogYears + " "+ "in dog years." );
+
+
+//Slice of pie pt.1
+
+var slices; 
+var spp = 8; //slices per pizza
+var pap = 16; //people at party
+var po = 5; //pizzas order
+
+var slices = spp * po / pap;
+
+//print out number number of slices each person ate
+console.log("Each person ate "+ slices +" slices of pizza at the party.");
+
+
+//Slice of pie pt.2
+
+var slices;
+var spp = 8; //slices per pizza
+var pap = 16; //people at party
+var po = 5; //pizzas order
+
+var spark = spp * po;
+
+
+//Sparky got x slices of pizza
+console.log("Sparky got"+" " + spark + " " +"slices of pizza.");
+
+
+
