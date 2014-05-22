a1e34d159564bfd8b1749253ceed1884c31e7ce6
Mirajo <mmj_chicago@yahoo.com>
2014-05-22 15:20:02 -0400

    Pounds per day
    Pounds per day

--- Wk2/Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Wk2/Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -3,11 +3,16 @@
 //Conditionals Assignment WPF
 
 
-var cWeight = prompt("What is your weight ?");//Input your current weight
-var gWeight = prompt("What is your goal weight ?")//Input your goal/ideal weight
-
-
-
+var cWeight = Number(prompt("What is your weight ?"));    //Input your current weight
+var gWeight = Number(prompt("What is your goal weight ?"));   //Input your goal/ideal weight
+var dWorkedout = Number(prompt("How many days have you worked out ?")); //Input days you've exercised
+var lWeight = Number(prompt("How many pounds have you lost ?")); //Input total pounds yov've lost
+console.log(cWeight);
+console.log(gWeight);
+console.log(dWorkedout);
+console.log(lWeight);
+var perDay = lWeight / dWorkedout;
+console.log(perDay);
 
 
 
