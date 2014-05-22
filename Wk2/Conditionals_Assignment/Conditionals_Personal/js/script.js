aa453d309032e535b1259b08050290a64f2f6370
Mirajo <mmj_chicago@yahoo.com>
2014-05-22 19:01:06 -0400

    text for console log print outs
    text for console log print outs

--- Wk2/Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Wk2/Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -3,24 +3,30 @@
 //Conditionals-Personal Assignment WPF
 
 
+// Workout days until weight loss goal
+
 var cWeight = Number(prompt("What is your weight ?"));    //Input your current weight
-console.log(cWeight);  //Print out current weight
+console.log("Your current weight is" +" " + cWeight + "lbs.");  //Print out current weight
 
 var gWeight = Number(prompt("What is your goal weight ?"));   //Input your goal/ideal weight
-console.log(gWeight); //Print out goal weight
+console.log("Your goal weight is" +" " + gWeight + "lbs."); //Print out goal weight
 
 //var ptLose = cWeight - gWeight;
 //console.log(ptLose);
 
 var dWorkedout = Number(prompt("How many days have you worked out ?")); //Input days you've exercised
-console.log(dWorkedout); //Print out total days worked out
+console.log("You have worked out" + " "+ dWorkedout + " " + "days."); //Print out total days worked out
 
 var lWeight = Number(prompt("How many pounds have you lost ?")); //Input total pounds you've lost
-console.log(lWeight); //Print out total pounds lost
+console.log("You have lost" + " " + lWeight + "lbs."); //Print out total pounds lost
 
 var perDay = lWeight / dWorkedout; //Total pounds you've lost per day.
-console.log(perDay);  //Print out the total pounds you've lost per day.
+console.log("Your weight loss rate is" + " " + perDay + "lbs. per day");  //Print out the total pounds you've lost per day.
 
 var wlDays = gWeight / perDay; //Today days till weight loss goal.
 console.log(wlDays);  //Print out total days until weight loss goal
 
+if(perDay != " "){
+//
+console.log("Your daily weight loss is on target. Good Job !");
+}
