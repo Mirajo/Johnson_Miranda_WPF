2ed7232d421f37da0b52e2c041e558fad08a446d
Mirajo <mmj_chicago@yahoo.com>
2014-05-22 16:34:28 -0400

    pounds to lose variable
    pounds to lose variable

--- Wk2/Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Wk2/Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -5,6 +5,8 @@
 
 var cWeight = Number(prompt("What is your weight ?"));    //Input your current weight
 var gWeight = Number(prompt("What is your goal weight ?"));   //Input your goal/ideal weight
+var ptLose = cWeight - gWeight;
+console.log(ptLose);
 var dWorkedout = Number(prompt("How many days have you worked out ?")); //Input days you've exercised
 var lWeight = Number(prompt("How many pounds have you lost ?")); //Input total pounds you've lost
 console.log(cWeight);
