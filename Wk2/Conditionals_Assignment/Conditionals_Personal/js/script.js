157211b570426b43b2e230ddbbacde102def12a0
Mirajo <mmj_chicago@yahoo.com>
2014-05-22 14:01:42 -0400

    conditionals personal variables
    conditionals personal variables

--- Wk2/Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Wk2/Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -3,6 +3,9 @@
 //Conditionals Assignment WPF
 
 
+var cWeight = prompt("What is your weight ?");//Input your current weight
+var gWeight = prompt("What is your goal weight ?")//Input your goal/ideal weight
+
 
 
 
