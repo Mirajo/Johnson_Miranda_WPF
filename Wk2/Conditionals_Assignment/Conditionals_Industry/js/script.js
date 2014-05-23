0012545e067eba2b515b9014663b0aad7a47d871
Mirajo <mmj_chicago@yahoo.com>
2014-05-22 22:07:15 -0400

    error with prompt
    error with prompt

--- Wk2/Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Wk2/Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -7,7 +7,7 @@
 // User age, login & Registration
 
 var minAge = 13; //minimum age for registration
-var userAge = prompt("Enter your age");  //actual age of user
+var userAge = (prompt("Enter your age"));  //actual age of user
 var maxAge = 100; //maximum age allowed for registration
 
 
