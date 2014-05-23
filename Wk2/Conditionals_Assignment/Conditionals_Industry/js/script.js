733321c674e8a25d86e8f2fb8ea0e0ca455dd3fd
Mirajo <mmj_chicago@yahoo.com>
2014-05-22 21:29:26 -0400

    adding conditionals variables
    adding conditionals variables

--- Wk2/Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Wk2/Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -6,4 +6,12 @@
 
 // User age, login & Registration
 
+var minAge = 13; //minimum age for registration
+var userAge = prompt("Enter your age");  //actual age of user
+var maxAge = 100; //maximum age allowed for registration
+
+if
+
+
+
 
