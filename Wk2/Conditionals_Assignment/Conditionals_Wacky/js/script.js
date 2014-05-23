862213db3a837affe585c5106b04f1de922d9df6
Mirajo <mmj_chicago@yahoo.com>
2014-05-22 22:57:51 -0400

    folders renamed
    folders renamed

--- Wk2/Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++
@@ -1,5 +0,0 @@
-//Miranda Johnson
-//05-22-2014
-//Conditionals-Wacky Assignment WPF
-
-
---
+++ Wk3/Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -0,0 +1,13 @@
+//Miranda Johnson
+//05-22-2014
+//Conditionals-Wacky Assignment WPF
+
+
+//stilts at the door
+
+var height = prompt("Enter your height"); //user must enter their actual height
+console.log("You are" + " " + height + " "+ "feet tall.");  //print out your actual height
+var stilts = (4 + "  " + "feet tall.") //variable for the height of the stilts
+console.log("The stilts are" + " "+ stilts); //print out the height of the stilts
+var wStilts = height - stilts; //variable for height without stilts
+console.log(wStilts); //print out the height with stilts
