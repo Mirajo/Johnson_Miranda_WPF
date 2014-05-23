5bbc0cca20ae62531f2d26b2b86b48d2d1646c21
Mirajo <mmj_chicago@yahoo.com>
2014-05-22 21:39:25 -0400

    read and agree to terms
    user 13 and and over must read and agree to terms for registration
    approval

--- Wk2/Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Wk2/Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -10,7 +10,16 @@ var minAge = 13; //minimum age for registration
 var userAge = prompt("Enter your age");  //actual age of user
 var maxAge = 100; //maximum age allowed for registration
 
-if
+
+if(userAge < minAge){ console.log("You must be 13 years or older to join this site.")
+}
+	else(userAge => minAge { 
+	//print out that all user must read and agree to terms for website registration
+	console.log("You must read and agree to the terms for registration approval.")
+}
+	else if(userAge == maxAge) {
+
+	console.log("You are 
 
 
 
