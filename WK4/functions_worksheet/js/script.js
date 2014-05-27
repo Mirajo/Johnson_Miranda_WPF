2a14d8c067719dc39cbdc9615d4a6769e171312a
Mirajo <mmj_chicago@yahoo.com>
2014-05-27 03:11:16 -0400

    functions worksheet initial commit
    functions worksheet initial commit

---
+++ WK4/functions_worksheet/js/script.js
@@ -0,0 +1,24 @@
+//Functions Worksheet WPF Activity
+
+
+
+//Calculate the area of a rectangle
+
+
+
+
+
+
+//calculate the circumference of a circle
+
+
+
+//calculate how many bee stings are needed
+
+
+
+
+
+
+
+
