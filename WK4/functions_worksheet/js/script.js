ce71c1f2f76561738261a3eca922a4e0f1f80cd9
Mirajo <mmj_chicago@yahoo.com>
2014-05-27 21:00:20 -0400

    circle circumference
    circle circumference prints to the console log

--- WK4/functions_worksheet/js/script.js
+++ WK4/functions_worksheet/js/script.js
@@ -4,24 +4,26 @@
 //calculate the circumference of a circle
 
 //Define function
-var cirCir = function(p,d){
+var cirCir = function(p, d){
 	//Run function
 	var cir = p * d;
 	//Return function
 	return cir;
-	}
+}
+
 //Enter circle diameter	
 var cirDia = cirCir(3.14, prompt("Enter the diameter of the circle."));
 
-console.log("The circumference of the circle is" + cirDia ");	
+//Print to the console
+console.log("The circumference of the circle is" +  " " + cirDia );	
 
 
 
 //calculate how many bee stings are needed
 
-var bStings = function
+var bStings = function(){
 
-
+}
 
 
 
