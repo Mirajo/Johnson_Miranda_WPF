5ed2c0df01201864d65716263ca1b30a0f520f35
Mirajo <mmj_chicago@yahoo.com>
2014-05-27 20:18:30 -0400

    B stings
    B stings

--- WK4/functions_worksheet/js/script.js
+++ WK4/functions_worksheet/js/script.js
@@ -1,14 +1,6 @@
 //Functions Worksheet WPF Activity
 
 
-
-//Calculate the area of a rectangle
-
-
-
-
-
-
 //calculate the circumference of a circle
 
 //Define function
@@ -23,9 +15,11 @@ var cir = cirCir(3.14, prompt("Enter the diameter of the circle."));
 
 //console.log("The circumference of the circle is " + cir ");	
 
+
+
 //calculate how many bee stings are needed
 
-
+var bStings = function
 
 
 
