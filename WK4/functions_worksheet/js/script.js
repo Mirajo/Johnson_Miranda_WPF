d1ced7f0e6ca452e34ab25473b6b5fe750126c71
Mirajo <mmj_chicago@yahoo.com>
2014-05-27 20:03:07 -0400

    define, run and return
    define, run and return

--- WK4/functions_worksheet/js/script.js
+++ WK4/functions_worksheet/js/script.js
@@ -11,7 +11,13 @@
 
 //calculate the circumference of a circle
 
+//Define function
 var cirCir = function(p,d){
+	//Run function
+	var cir = p * d;
+	//Return function
+	return cir;
+	}
 
 //calculate how many bee stings are needed
 
