fd68ead2c8c16529928f8e22f3615bdfaeffdfec
Mirajo <mmj_chicago@yahoo.com>
2014-05-29 17:09:44 -0400

    console print out entries
    console print out enteries

--- WK4/functions_Assignment/functions_Industry/js/script.js
+++ WK4/functions_Assignment/functions_Industry/js/script.js
@@ -1,15 +1,18 @@
 //Miranda Johnson
 //05-29-14
-//Functions Industry WPF Assignment
+//Functions - Industry WPF Assignment
 
 
 //Office/Work Budget/Debt
 
-var cAmt = prompt("How much do you currently have for expenses ?");//Enter the amount you currently have for the budget
-console.log("You currently have " + " "+ cAmt + " " + "for expenses.")
+var cAmt = prompt("How much do you currently have for expenses ?");//Enter the amount you currently have for expenses
+console.log("You currently have" +" "+ cAmt +" "+ "for expenses."); //Print the amount you have for expenses
 var hAmt = prompt("What is the amount you spend on monthly hosting ?"); //Enter the amount you spend on hosting monthly
+console.log("You currently have" +" "+ hAmt +" "+ "for hosting expenses."); //Print the amount you have spent for monthly hosting
 var aAmt = prompt("What is the amount you spend on monthly advertising ?"); //Enter the amount you spend on advertising monthly
+console.log("You currently have" +" "+ aAmt +" "+ "for advertising expenses."); //Print the amount you have spent for monthly advertising
 var oAmt = prompt("What is the amount you spend for office space?"); //Enter the amount you spend for office space rental
+
 var lAmt = prompt("What is the amount you spend on software licenses ?"); //Enter the amount you spend for software licenses
 var ssAmt = prompt("What is the amount you spend to pay staff ?"); //Enter the amount you spend to pay staff salary
 var osAmt = prompt("What is the amount you spend to pay your own salaray ?"); //Enter the amount you spend to pay yourself
