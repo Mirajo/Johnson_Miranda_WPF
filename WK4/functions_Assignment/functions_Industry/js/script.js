eb4819c28594feba925bc141080095fab3b9c85a
Mirajo <mmj_chicago@yahoo.com>
2014-05-29 16:38:28 -0400

    Functions Industry office
    Functions Industry office

--- WK4/functions_Assignment/functions_Industry/js/script.js
+++ WK4/functions_Assignment/functions_Industry/js/script.js
@@ -1,5 +1,14 @@
+//Miranda Johnson
+//05-29-14
 //Functions Industry WPF Assignment
 
 
-//calculate the circumference of a circle
+//Office/Work Budget
+
+var cAmt = prompt("How much do you currently have for expenses ?");//Enter the amount you currently have for the budget
+var hAmt = prompt("What is the amount you spend on monthly hosting ?")//Enter the amount you spend on hosting monthly
+var aAmt = prompt("What is the amount you spend on monthly advertising ?")//Enter the amount you spend on advertising monthly
+var OAmt = promopt("What is the amount you spend for office space?");//What is the amount you spend for office space rental
+
+
 
