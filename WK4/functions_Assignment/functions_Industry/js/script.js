319cbc64a82481912289c557c5b3e824c82b5bbf
Mirajo <mmj_chicago@yahoo.com>
2014-05-29 16:55:27 -0400

    functions variables
    functions variables

--- WK4/functions_Assignment/functions_Industry/js/script.js
+++ WK4/functions_Assignment/functions_Industry/js/script.js
@@ -6,9 +6,12 @@
 //Office/Work Budget
 
 var cAmt = prompt("How much do you currently have for expenses ?");//Enter the amount you currently have for the budget
-var hAmt = prompt("What is the amount you spend on monthly hosting ?")//Enter the amount you spend on hosting monthly
-var aAmt = prompt("What is the amount you spend on monthly advertising ?")//Enter the amount you spend on advertising monthly
-var OAmt = promopt("What is the amount you spend for office space?");//What is the amount you spend for office space rental
+var hAmt = prompt("What is the amount you spend on monthly hosting ?"); //Enter the amount you spend on hosting monthly
+var aAmt = prompt("What is the amount you spend on monthly advertising ?"); //Enter the amount you spend on advertising monthly
+var oAmt = prompt("What is the amount you spend for office space?"); //Enter the amount you spend for office space rental
+var lAmt = prompt("What is the amount you spend on software licenses ?"); //Enter the amount you spend for software licenses
+var ssAmt = prompt("What is the amount you spend to pay staff ?"); //Enter the amount you spend to pay staff salary
+var osAmt = prompt("What is the amount you spend to pay your own salaray ?"); //Enter the amount you spend to pay yourself
 
 
 
