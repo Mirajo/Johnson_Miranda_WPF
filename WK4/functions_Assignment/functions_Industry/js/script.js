a6551709cb4e5f05c02143cc8e957c6921f14fae
Mirajo <mmj_chicago@yahoo.com>
2014-05-29 17:16:36 -0400

    variables and console added
    variables and console added

--- WK4/functions_Assignment/functions_Industry/js/script.js
+++ WK4/functions_Assignment/functions_Industry/js/script.js
@@ -7,13 +7,23 @@
 
 var cAmt = prompt("How much do you currently have for expenses ?");//Enter the amount you currently have for expenses
 console.log("You currently have" +" "+ cAmt +" "+ "for expenses."); //Print the amount you have for expenses
+
+var cmAmt = (cAmt / 12)
+console.log("You have" +" "+ cmAmt +" "+ "per month for expenses."); //Print the amount you have per month for expenses
+
+
 var hAmt = prompt("What is the amount you spend on monthly hosting ?"); //Enter the amount you spend on hosting monthly
-console.log("You currently have" +" "+ hAmt +" "+ "for hosting expenses."); //Print the amount you have spent for monthly hosting
+console.log("You've spent" +" "+ hAmt +" "+ "for hosting expenses this month."); //Print the amount you have spent for monthly hosting
+
 var aAmt = prompt("What is the amount you spend on monthly advertising ?"); //Enter the amount you spend on advertising monthly
-console.log("You currently have" +" "+ aAmt +" "+ "for advertising expenses."); //Print the amount you have spent for monthly advertising
+console.log("You've spent" +" "+ aAmt +" "+ "for advertising expenses this month."); //Print the amount you have spent for monthly advertising
+
 var oAmt = prompt("What is the amount you spend for office space?"); //Enter the amount you spend for office space rental
+console.log("You've spent" +" "+ oAmt +" "+ "for office space rental this month."); //Print the amount you have spent for monthly office space rental
 
 var lAmt = prompt("What is the amount you spend on software licenses ?"); //Enter the amount you spend for software licenses
+console.log("You've spent" +" "+ lAmt +" "+ "software licenses this month."); //Print the amount you have spent for monthly software licenses
+
 var ssAmt = prompt("What is the amount you spend to pay staff ?"); //Enter the amount you spend to pay staff salary
 var osAmt = prompt("What is the amount you spend to pay your own salaray ?"); //Enter the amount you spend to pay yourself
 var iAmt = prompt("What is the amount you have received from investors ?"); //Enter the amount received from investors
