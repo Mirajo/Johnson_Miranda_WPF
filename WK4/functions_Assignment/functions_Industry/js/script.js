5349ceda0fca3c964fe5928e076a07f68ea9fe87
Mirajo <mmj_chicago@yahoo.com>
2014-05-29 17:46:15 -0400

    print expenses
    print expenses var for Currently monthly amount added

--- WK4/functions_Assignment/functions_Industry/js/script.js
+++ WK4/functions_Assignment/functions_Industry/js/script.js
@@ -8,8 +8,8 @@
 var cAmt = prompt("How much do you currently have for expenses ?");//Enter the amount you currently have for expenses
 console.log("You currently have" +" "+ cAmt +" "+ "for expenses."); //Print the amount you have for expenses
 
-var cmAmt = cAmt / 12
-//console.log("You have" +" "+ cmAmt +" "+ "per month for expenses."); //Print the amount you have per month for expenses
+var cmAmt = parseInt(cAmt) / 12; 
+console.log("You have" +" "+ cmAmt +" "+ "per month for expenses."); //Print the amount you have per month for expenses
 
 //var hAmt = prompt("What is the amount you spend on monthly hosting ?"); //Enter the amount you spend on hosting monthly
 //console.log("You've spent" +" "+ hAmt +" "+ "for hosting expenses this month."); //Print the amount you have spent for monthly hosting
@@ -23,17 +23,17 @@ var cmAmt = cAmt / 12
 //var lAmt = prompt("What is the amount you spend on software licenses ?"); //Enter the amount you spend for software licenses
 //console.log("You've spent" +" "+ lAmt +" "+ "software licenses this month."); //Print the amount you have spent for monthly software licenses
 
-var ssAmt = prompt("What is the amount you spend to pay staff ?"); //Enter the amount you spend to pay staff salary
-console.log("You've spent" +" "+ ssAmt +" "+ "for staff salary."); //Print the amount you have spent for monthly office space rental
+//var ssAmt = prompt("What is the amount you spend to pay staff ?"); //Enter the amount you spend to pay staff salary
+//console.log("You've spent" +" "+ ssAmt +" "+ "for staff salary this month."); //Print the amount you have spent for monthly office space rental
 
-var osAmt = prompt("What is the amount you spend to pay your own salary ?"); //Enter the amount you spend to pay yourself
-console.log("You've spent" +" "+ osAmt +" "+ "for your own salary."); //Print the amount you have spent this month for your salary
+//var osAmt = prompt("What is the amount you spend to pay your own salary ?"); //Enter the amount you spend to pay yourself
+//console.log("You've spent" +" "+ osAmt +" "+ "for your own salary this month."); //Print the amount you have spent this month for your salary
 
-var iAmt = prompt("What is the amount you have received from investors ?"); //Enter the amount received from investors
-console.log("You've received" +" "+ iAmt +" "+ "from investors."); //Print the amount you have received from investors
+//var iAmt = prompt("What is the amount you have received from investors ?"); //Enter the amount received from investors
+//console.log("You've received" +" "+ iAmt +" "+ "from investors this month."); //Print the amount you have received from investors
 
-var bAmt = prompt("What is the amount you have borrowed to stay in business ?"); //Enter the amount you've borrowed
-console.log("You've spent" +" "+ bAmt +" "+ "for office space rental."); //Print the amount you have spent for monthly office space rental
+//var bAmt = prompt("What is the amount you have borrowed to stay in business ?"); //Enter the amount you've borrowed
+//console.log("You've borrowed" +" "+ bAmt +" "+ "to stay in business."); //Print the amount you have borrowed to stay in business
 
 
 
