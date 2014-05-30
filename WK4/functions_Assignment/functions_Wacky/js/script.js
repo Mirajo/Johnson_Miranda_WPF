d1a42b80be6ac30dda1fad73fadfd39277b35ab9
Mirajo <mmj_chicago@yahoo.com>
2014-05-29 22:46:18 -0400

    ternary, console message
    ternary, console message

--- WK4/functions_Assignment/functions_Wacky/js/script.js
+++ WK4/functions_Assignment/functions_Wacky/js/script.js
@@ -6,12 +6,21 @@
 //Silver dollars
 
 var tSd = oSd(Number(prompt("How many people ? ")), Number(prompt("How many silver dollars ?")));
-console.log(tsd);
-
-//function osd(tNp,tNs){
+//Chances of getting a silver dollar
+function oSd(tNp,tNs) {
 	//Odds
-    var odds = tnp / tns;
+    var odds = tNp / tNs;
     //return odds
-    return odds;
+    return odds; 
     }
+console.log("You have a 1 in" + " "+ tSd + " " + "chance of getting a silver dollar"); //Print out the odds of getting a silver dollar
+
+var message; //message for the odds of getting a silver dollar
+
+var pch = 1/500; //min for poor chance of getting silver dollar
+
+//set the odds
+message = (1/tSd > pch) ? "You have great odds ! " ; "The odds are not great.";
+//Print out the message
+console.log(message);
 
