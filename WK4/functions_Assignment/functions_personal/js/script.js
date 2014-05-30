d5af7a08e7d5c04c750f6bd51b4b6e0b27a8e6aa
Mirajo <mmj_chicago@yahoo.com>
2014-05-29 21:32:05 -0400

    added attendee variables
    added party attendee variables

--- WK4/functions_Assignment/functions_personal/js/script.js
+++ WK4/functions_Assignment/functions_personal/js/script.js
@@ -3,5 +3,11 @@
 //Functions - Personal WPF Assignment
 
 
-//
+//Party attendees
+
+var pAten = prompt("How many attendees are entering ?"); //Enter number of attendees entering
+var ppAten = prompt("How many attendees are present ?"); //Enter number of attendees present
+
+var pMax = 300; //Maximun capacity for event
+var mPh = parseInt(pMax) /
 
