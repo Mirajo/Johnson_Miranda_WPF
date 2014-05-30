af70ca63aaa21d3a42b3cb00aa620c2b36ab5e01
Mirajo <mmj_chicago@yahoo.com>
2014-05-29 21:47:15 -0400

    added party attendee function
    added party attendee function

--- WK4/functions_Assignment/functions_personal/js/script.js
+++ WK4/functions_Assignment/functions_personal/js/script.js
@@ -5,9 +5,13 @@
 
 //Party attendees
 
-var pAten = prompt("How many attendees are entering ?"); //Enter number of attendees entering
-var ppAten = prompt("How many attendees are present ?"); //Enter number of attendees present
+var pAten = prompt(Number("How many attendees are entering ?")); //Enter number of attendees entering
+var ppAten = prompt(Number("How many attendees are present ?")); //Enter number of attendees present
 
 var pMax = 300; //Maximun capacity for event
-var mPh = parseInt(pMax) /
+var mPh = parseInt(pMax) / 60; //Calculate maximum attendees permitted per min
+console.log("The maximum number of attendees permitted per min is" + " " + mPh);
 
+function tpAten(pAten, ppAten){
+ return parseInt(pAten) + parseInt(ppAten); //Return total number of party attendees
+ }
